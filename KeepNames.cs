daf687d2cee9df41db164bead5cd25a03d18fe9d
MyristicaFragrans <17411952+MyristicaFragrans@users.noreply.github.com>
2020-07-05 22:07:48 -0600

    Add Mod.Call support

--- KeepNames.cs
+++ KeepNames.cs
@@ -1,4 +1,5 @@
 using Steamworks;
+using System;
 using System.Collections.Generic;
 using Terraria.ModLoader;
 
@@ -43,6 +44,55 @@ namespace KeepNames {
 			else {
 				return names[i].givenName;
             }
+		}
+
+		/// <summary>
+		/// Dynamically change listings and blacklists in KeepNames
+		/// Returns true on success, false on fail
+		/// Prints to log errors
+		/// </summary>
+		/// <example>Call( "blacklistNPC", NPCType<MyNPC>() );</example>
+		/// <example>Call( "setName", NPCType<MyNPC>(), "MyNewName" );</example>
+		/// <example>Call( "getSavedName", NPCType<MyNPC>() );</example>
+		/// <param name="args">An object array of {"Method Name", [args in order]}</param>
+		public override object Call(params object[] args) {
+			if (args.Length < 2) {
+				Logger.Error("Mod.Call() expects at least 2 arguments.");
+				return false;
+			}
+			try {
+				string functionName = args[0] as string;
+				switch (functionName) {
+					case "blacklistNPC": {
+							int? id = args[1] as int?;
+							if (id == null) { Logger.Error("Second argument of blacklistNPC must be an int."); return false; }
+							blacklistNPC((int)id);
+							return true;
+						}
+					case "setName": {
+							if (args.Length < 3) { Logger.Error("Mod.Call() expects at least 3 arguments."); return false; }
+							int? id = args[1] as int?;
+							if (id == null) { Logger.Error("Second argument of setName must be an int."); return false; }
+							string name = args[2] as string;
+							if (name == null) { Logger.Error("Third argument of setName must be a string."); return false; }
+							setName((int)id, name);
+							return true;
+						}
+					case "getSavedName": {
+							int? id = args[1] as int?;
+							if (id == null) { Logger.Error("Second argument of getSavedName must be an int."); return false; }
+							getSavedName((int)id);
+							return true;
+						}
+					default:
+						Logger.Warn($"Call Warning: Unknown method name '{functionName}'");
+						return false;
+				}
+			}
+			catch (Exception e) {
+				Logger.Error($"Call Error: {e.StackTrace} {e.Message}");
+				return false;
+			}
 		}
 	}
 }
