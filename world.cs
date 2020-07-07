491c64ccb9c0bddb4c61d95a36ed410893c2343d
MyristicaFragrans <17411952+MyristicaFragrans@users.noreply.github.com>
2020-07-07 17:22:15 -0600

    Fix All Instances of typo "Persistant"

--- world.cs
+++ world.cs
@@ -26,7 +26,7 @@ namespace KeepNames {
 		public override void Load(TagCompound tag) {
 			List<int> ids = tag.Get<List<int>>("ids");
 			List<string> names = tag.Get<List<string>>("names");
-            if (ids.Count != names.Count) throw new System.Exception("Persistant Names: Mismatch between ids and names in NBT data");
+            if (ids.Count != names.Count) throw new System.Exception("Persistent Names: Mismatch between ids and names in NBT data");
 			KeepNames.names = new List<name> { };
 			for(var i = 0; i < ids.Count; i++) {
 				if(names[i]!="")
