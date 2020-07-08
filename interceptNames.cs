00d9e92166c10201fe3dab6ff58d5d46fdc80c52
Ronan Finley <17411952+RonanFinley@users.noreply.github.com>
2020-07-08 01:04:38 -0600

    Remove old SetDefaults, Make PreAI fallback of our hook

--- interceptNames.cs
+++ interceptNames.cs
@@ -5,17 +5,11 @@ using static Terraria.ModLoader.ModContent;
 
 namespace KeepNames {
     class interceptNames : GlobalNPC {
-        public override void SetDefaults(NPC npc) {
-            if (!KeepNames.blacklist.Contains(npc.type) && npc.townNPC && GetInstance<nameConfigServer>().manualBlackList.FindIndex(b => b.Type == npc.type) == -1) {
-                int i = KeepNames.names.FindIndex(obj => obj.id == npc.type);
-                if (i != -1) {
-                    npc.GivenName = KeepNames.names[i].givenName;
-                }
-            }
-            base.SetDefaults(npc);
-        }
         public override bool PreAI(NPC npc) {
-            if (!KeepNames.blacklist.Contains(npc.type) && npc.townNPC && GetInstance<nameConfigServer>().manualBlackList.FindIndex(b => b.Type == npc.type)==-1) {
+            if (!KeepNames.patchedGame
+                && !KeepNames.blacklist.Contains(npc.type)
+                && npc.townNPC
+                && GetInstance<nameConfigServer>().manualBlackList.FindIndex(b => b.Type == npc.type) == -1) {
                 int i = KeepNames.names.FindIndex(obj => obj.id == npc.type);
                 if (i != -1) {
                     npc.GivenName = KeepNames.names[i].givenName;
