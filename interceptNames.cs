3beda37ec31e61277183c0d60093efb3a40c9e72
Ronan Finley <17411952+RonanFinley@users.noreply.github.com>
2020-07-10 22:58:10 -0600

    Add checks for Servers

--- interceptNames.cs
+++ interceptNames.cs
@@ -9,7 +9,9 @@ namespace KeepNames {
             if (!KeepNames.patchedGame
                 && !KeepNames.blacklist.Contains(npc.type)
                 && npc.townNPC
-                && GetInstance<nameConfigServer>().manualBlackList.FindIndex(b => b.Type == npc.type) == -1) {
+                && GetInstance<nameConfigServer>().manualBlackList.FindIndex(b => b.Type == npc.type) == -1
+                //exit if we are not either the host or in singleplayer
+                && !(Terraria.Main.netMode == Terraria.ID.NetmodeID.MultiplayerClient && !Terraria.Main.dedServ)) {
                 int i = KeepNames.names.FindIndex(obj => obj.id == npc.type);
                 if (i != -1) {
                     npc.GivenName = KeepNames.names[i].givenName;
