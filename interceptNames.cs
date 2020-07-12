bba6e154426ee9f8c70e224b6d953a773d6d6b0c
MyristicaFragrans <17411952+MyristicaFragrans@users.noreply.github.com>
2020-07-12 15:22:17 -0600

    Refactor logic to allow names to be saved when patch succeeded

--- interceptNames.cs
+++ interceptNames.cs
@@ -1,25 +1,26 @@
-﻿using System;
-using Terraria;
+﻿using Terraria;
 using Terraria.ModLoader;
 using static Terraria.ModLoader.ModContent;
 
 namespace KeepNames {
     class interceptNames : GlobalNPC {
         public override bool PreAI(NPC npc) {
-            if (!KeepNames.patchedGame
-                && !KeepNames.blacklist.Contains(npc.type)
-                && npc.townNPC
+            if (!KeepNames.blacklist.Contains(npc.type)
+                && ( npc.townNPC || KeepNames.considerAsTownNPCs.Contains(npc.type) )
                 && GetInstance<nameConfigServer>().manualBlackList.FindIndex(b => b.Type == npc.type) == -1
                 //exit if we are not either the host or in singleplayer
-                && !(Terraria.Main.netMode == Terraria.ID.NetmodeID.MultiplayerClient && !Terraria.Main.dedServ)) {
-                int i = KeepNames.names.FindIndex(obj => obj.id == npc.type);
-                if (i != -1) {
-                    npc.GivenName = KeepNames.names[i].givenName;
-                }
-                else if (npc.townNPC && !KeepNames.blacklist.Contains(npc.type) && npc.GivenName!="") {
+                && !(Main.netMode != Terraria.ID.NetmodeID.SinglePlayer && !Main.dedServ)) {
+                if(!KeepNames.patchedGame) {
+                    int i = KeepNames.names.FindIndex(obj => obj.id == npc.type);
+                    if (i != -1) {
+                        npc.GivenName = KeepNames.names[i].givenName;
+                    }
+                } else if ( npc.GivenName != "" ) {
                     KeepNames.setName(npc.type, npc.GivenName);
                 }
+
             }
+            
             return base.PreAI(npc);
         }
     }
